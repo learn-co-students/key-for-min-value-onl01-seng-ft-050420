cd2e72316bb88e2090bd362a72c87916716c5e13
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 21:46:51 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,3 +8,19 @@ def key_for_min_value(name_hash)
   name_hash.each {|key, value| min == nil || value < min ? (min = value; min_name = key) : min_name}
   min_name
 end
+
+
+=begin
+def key_for_min_value(name_hash) 
+  min = nil
+  min_name = nil
+  
+  name_hash.each do |key, value|
+    if min == nil || value < min
+      min = value
+      min_name = key
+    end
+  end
+  min_name
+end
+=end
