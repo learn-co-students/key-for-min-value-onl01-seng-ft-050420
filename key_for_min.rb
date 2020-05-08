67784f2a5f39b954dd2fa756686abe6bb7f2186b
rylandgrounds <rylandgrounds@gmail.com>
2020-05-08 14:16:34 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+require "pry"
 
-end
+def key_for_min_value(hash)
+    hash.inject { |k,v| k[1] > v[1] ? v : k}[0] unless hash.empty?
+  end
